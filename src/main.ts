39ad589ecccb818484bcc551aefa31fe74791074
Tokoloten <endermen96@gmail.com>
2026-09-13 23:12:06 +0900

    feat: first commit for github pages by Action

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-import { Viewer } from 'cesium';
+import { Color, Viewer, GeoJsonDataSource } from 'cesium';
 import 'cesium/Build/Cesium/Widgets/widgets.css';
 
 const viewer = new Viewer('cesiumContainer');
+const coastData = await GeoJsonDataSource.load(
+    import.meta.env.BASE_URL + "data/base/coastline/Hya_0kai.geojson",
+    {
+        stroke: Color.BLACK, 
+        fill: Color.GREEN.withAlpha(0.4), 
+        strokeWidth: 2
+});
+viewer.dataSources.add(coastData)
